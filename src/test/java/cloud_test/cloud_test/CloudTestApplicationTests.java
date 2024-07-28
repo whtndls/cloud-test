d1b99e5efb55c36002a97f14ca2e97ee525af1ed
SUIN <65757661+whtndls@users.noreply.github.com>
2024-07-28 22:51:10 +0900

    init commit

---
+++ src/test/java/cloud_test/cloud_test/CloudTestApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud_test.cloud_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
